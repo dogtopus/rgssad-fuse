79b64768b31df5e3b753eb5150f22b51ae727d5b
dogtopus <dogtopus@users.noreply.github.com>
2018-10-25 22:58:07 -0300

    Experimental seek algorithm that has log time complexity (!)
    Forward/backward decision is broken. Should use hamming weight of block
    distance instead of just block distance.

--- rgssad/crypto.py
+++ rgssad/crypto.py
@@ -17,11 +17,48 @@
 import struct
 import logging
 
+# https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+def xgcd(a, m):
+    s, t, old_s, old_t = 0, 1, 1, 0
+    r, old_r = m, a
+    while r != 0:
+        q = old_r // r
+        old_r, r = r, old_r - q*r
+        old_s, s = s, old_s - q*s
+        old_t, t = t, old_t - q*t
+    # we don't care about the another half of the Bézout coefficients
+    return old_r, old_s
+
+# calculates equivalent LCG constants after 2^n iterations
+# WARNING: costly, do not invoke too frequently
+def lcg_gen(mul, add, bits):
+    gcd, im = xgcd(mul, 1 << 32)
+    if gcd != 1:
+        im = None
+    else:
+        im &= 0xffffffff
+    yield mul, add, im
+    m, a = mul, add
+    for i in range(1, bits):
+        m2 = m * m
+        a2 = a * (m+1)
+        m, a = m2 & 0xffffffff, a2 & 0xffffffff
+        if im is not None:
+            im2 = im * im
+            im = im2 & 0xffffffff
+        yield m, a, im
+
+# Why 30? Remember that RGSSAD uses 32-bit offset and each block of data is 4
+# bytes.
+LCG_TABLE = tuple(lcg_gen(7, 3, 30))
+
 class MagicKeyFactory(object):
-    def __init__(self, iv=0xdeadcafe):
+    def __init__(self, iv=0xdeadcafe, lcg_table=LCG_TABLE):
+        self._lcg_table = lcg_table
         self.logger = logging.getLogger('rgssad.MagicKeyFactory')
         self.key = 0
         self.iv = iv
+        self.can_rewind = self._lcg_table[0][2] is not None
         self.reset()
 
     def get_key(self):
@@ -35,39 +72,49 @@ class MagicKeyFactory(object):
 
     def skip(self, count):
         self.logger.debug('skip %d block(s)', count)
-        # Transform 4 times in 1 multiplication and 1 addition to save some time
-        if count >= 4:
-            t4_count = count // 4
-            t_count = count % 4
-            
-            for i in range(t4_count):
-                self._transform4()
-            for i in range(t_count):
-                self._transform()
-        else:
-            for i in range(count):
-                self._transform()
+        self._transformn(count)
 
     def rewind(self, count):
         self.logger.debug('rewind %d block(s)', count)
-        for i in range(count):
-            self._transform_backwards()
+        self._transformn_backwards(count)
 
     def _transform(self):
-        self.key *= 7
-        self.key += 3
+        self.key *= self._lcg_table[0][0]
+        self.key += self._lcg_table[0][1]
         self.key &= 0xffffffff
 
     def _transform_backwards(self):
-        self.key -= 3
+        if not self.can_rewind:
+            raise TypeError('This factory does not support rewind')
+        self.key -= self._lcg_table[0][1]
         # 0xb6db6db7 = inv(7) (mod 0x100000000)
-        self.key *= 0xb6db6db7
+        self.key *= self._lcg_table[0][2]
         self.key &= 0xffffffff
 
-    def _transform4(self):
-        self.key *= 2401
-        self.key += 1200
-        self.key &= 0xffffffff
+    def _transformn_backwards(self, n):
+        if not self.can_rewind:
+            raise TypeError('This factory does not support rewind')
+        bitpos = 0
+        while n:
+            if n & 1:
+                _, a, im = LCG_TABLE[bitpos]
+                self.key -= a
+                self.key *= im
+                self.key &= 0xffffffff
+            n >>= 1
+            bitpos += 1
+
+    # Transform n times in lg(n) time complexity!
+    def _transformn(self, n):
+        bitpos = 0
+        while n:
+            if n & 1:
+                m, a, _ = LCG_TABLE[bitpos]
+                self.key *= m
+                self.key += a
+                self.key &= 0xffffffff
+            n >>= 1
+            bitpos += 1
 
     def one_step_rollback(self):
         self._transform_backwards()
